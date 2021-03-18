d81368261592409c3414d4cb804195a10ba3611d
Daniel Mohr <daniel.mohr@dlr.de>
2021-03-18 19:37:58 +0100

    tried to enhance the reimplementation of src/dabu/schema_org_data/

--- src/dabu/schema_org_data/json_schema_from_schema_org2.py
+++ src/dabu/schema_org_data/json_schema_from_schema_org2.py
@@ -1,13 +1,14 @@
 """
 :Author: Daniel Mohr
 :Email: daniel.mohr@dlr.de
-:Date: 2021-03-17 (last change).
+:Date: 2021-03-18 (last change).
 :License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007.
 """
 
 import json
 import sys
 
+from dabu.compare_json_schemas import compare_json_schemas_one_way
 
 def set_property(schema, key, value):
     """
@@ -88,13 +89,25 @@ def create_context_schema(word):
                                     "enum": ["https://schema.org/" + word]}}
     return context
 
+def _are_types_equal(type1, type2):
+    """
+    :Author: Daniel Mohr
+    :Date: 2021-03-18
+    """
+    iseq = True
+    try:
+        compare_json_schemas_one_way(type1, type2)
+    except AssertionError:
+        iseq = False
+    return iseq
 def create_properties_schema2json(
         properties, schema2json, prop_name, word, item_type):
     """
     :Author: Daniel Mohr
-    :Date: 2021-03-17
+    :Date: 2021-03-18
     """
     value = "https://schema.org/" + word
+    store_id = True
     if prop_name not in properties:
         properties[prop_name] = dict()
         store_prop = properties[prop_name]
@@ -105,15 +118,39 @@ def create_properties_schema2json(
                     (properties[prop_name]["@id"] == value)):
                     return
                 properties[prop_name] = {"oneOf": [properties[prop_name]]}
+                properties[prop_name]["oneOf"].append(dict())
+                store_prop = properties[prop_name]["oneOf"][-1]
             else: # properties[prop_name]["oneOf"] is list
+                def_store_prop = False
                 for item in properties[prop_name]["oneOf"]:
                     if (("@id" in item) and (item["@id"] == value)):
-                        return
-            properties[prop_name]["oneOf"].append(dict())
-            store_prop = properties[prop_name]["oneOf"][-1]
+                        print('xyz', '\n ', item["type"], '\n ', item, '\n ', schema2json[item_type])
+                        # _get_property identifier
+                        iseq = False
+                        if not "oneOf" in item["type"]:
+                            iseq = _are_types_equal(schema2json[item_type],
+                                                    item["type"])
+                        else:
+                            print('type(item["type"]["oneOf"])', type(item["type"]["oneOf"]))
+                            for litem in item["type"]["oneOf"]:
+                                print('litem', litem)
+                                iseq = _are_types_equal(schema2json[item_type],
+                                                        litem["type"])
+                        if iseq:
+                            return
+                        if not "oneOf" in item["type"]:
+                            item["type"] = {"oneOf": [{"type": item["type"]}]}
+                        item["type"]["oneOf"].append(dict())
+                        store_prop = item["type"]["oneOf"][-1]
+                        def_store_prop = True
+                        store_id = False
+                        break
+                if not def_store_prop:
+                    return
         else:
             raise NotImplementedError(json.dumps(data, indent=2))
-    store_prop["@id"] = value
+    if store_id:
+        store_prop["@id"] = value
     #item_type = data["schema:rangeIncludes"]["@id"].split('schema:')[1]
     if isinstance(schema2json[item_type], str):
         store_prop["type"] = schema2json[item_type]
@@ -122,6 +159,10 @@ def create_properties_schema2json(
             store_prop[key] = schema2json[item_type][key]
 def create_properties_handle(
         properties, prop_name, missing_words, word, item_type_ref):
+    """
+    :Author: Daniel Mohr
+    :Date: 2021-03-17
+    """
     value = "#/definitions/" + item_type_ref
     if prop_name not in properties:
         properties[prop_name] = dict()
@@ -145,6 +186,10 @@ def create_properties_handle(
     store_prop["$ref"] = value
     missing_words.append(item_type_ref)
 def _rangeincludes_list(data, schema2json, handle):
+    """
+    :Author: Daniel Mohr
+    :Date: 2021-03-17
+    """
     accept_list = []
     if not ("schema:rangeIncludes" in data):
         return None
@@ -160,7 +205,7 @@ def _get_property(item, data, schema2json, properties, prop_name,
                   missing_words):
     """
     :Author: Daniel Mohr
-    :Date: 2021-03-17
+    :Date: 2021-03-18
     """
     handle = ["ImageObject", "MediaObject",
               "Distance", "CreativeWork",
@@ -170,7 +215,11 @@ def _get_property(item, data, schema2json, properties, prop_name,
               "DataCatalog", "Dataset", "DataDownload",
               "Organization", "AdministrativeArea", "VirtualLocation",
               "EducationalOrganization", "Photograph", "CorrectionComment",
-              "AudioObject"]
+              "AudioObject", "Brand", "ContactPoint", "Language", "Occupation",
+              "hasOfferCatalog", "OfferCatalog", "InteractionCounter", "Offer",
+              "ProgramMembership", "Country", "MonetaryAmount",
+              "PriceSpecification", "OwnershipInfo", "Product", "Event",
+              "Demand", "QuantitativeValue", "PropertyValue"]
     word = data["@id"].split('schema:')[1]
     print('_get_property', word, '\n')
     if word in schema2json:
@@ -186,10 +235,12 @@ def _get_property(item, data, schema2json, properties, prop_name,
         create_properties_schema2json(
             properties, schema2json, word, prop_name,
             data["schema:rangeIncludes"]["@id"].split('schema:')[1])
-    elif item["@id"] in handle:
-        print('item["@id"] in handle', '\n')
+    elif item["@id"].split('schema:')[1] in handle:
+        print('item["@id"].split("schema:")[1] in handle', item["@id"].split('schema:')[1], '\n')
+        print('item', item, 'word', word)
+        print('data', data, '\n')
         create_properties_handle(
-            properties, prop_name, missing_words, word, word)
+            properties, prop_name, missing_words, word,  item["@id"].split('schema:')[1])
     elif (("schema:rangeIncludes" in data) and
           ("@id" in data["schema:rangeIncludes"]) and
           (data["schema:rangeIncludes"]["@id"].split('schema:')[1] in
@@ -207,13 +258,14 @@ def _get_property(item, data, schema2json, properties, prop_name,
                 print('accept_list', '\n')
                 for item_type in accept_list:
                     if item_type in schema2json:
-                        print('item_type in schema2json', '\n')
+                        print('item_type in schema2json', item_type, '\n')
                         create_properties_schema2json(
                             properties, schema2json, word, prop_name, item_type)
                     elif item_type in handle:
                         print('item_type in handle', '\n')
                         create_properties_handle(
-                            properties, prop_name, missing_words, word, item_type)
+                            properties, prop_name, missing_words,
+                            word, item_type)
             elif len(accept_list) == 1:
                 print('accept_list item', '\n')
                 item_type = accept_list[0]
@@ -233,7 +285,7 @@ def get_property(schemaorg_data, properties, prop_name, missing_words,
                  draft='draft-04'):
     """
     :Author: Daniel Mohr
-    :Date: 2021-03-17
+    :Date: 2021-03-18
 
     data:
 
@@ -257,15 +309,29 @@ def get_property(schemaorg_data, properties, prop_name, missing_words,
                    "Number": "number",
                    "Float": "number",
                    "URL": {"type": "string", "format": "uri"},
-                   "DateTime": {"type": "string", "format": "date-time"}}
+                   "DateTime": {"type": "string", "format": "date-time"},
+                   "Date": {"type": "string", "format": "date-time"},
+                   "Time": "string",
+                   "email": {"oneOf": [{"type": "string"},
+                                       {"type": "string", "format": "email"}]}}
     schema2json7 = {"Date": {"type": "string", "format": "date"},
                     "Time": {"type": "string", "format": "datetime"}}
+    if draft in ['draft-06']:
+        schema2json["URL"] = {
+            "oneOf": [{"type": "string", "format": "uri"},
+                      {"type": "string", "format": "uri-reference"}]}
     if draft in ['draft-07', '2019-09']:
-        for key in schema2json7:
-            schema2json[key] = schema2json7[key]
-    else:
-        for key in schema2json7:
-            schema2json[key] = "string"
+        schema2json["Date"] = {"type": "string", "format": "date"}
+        schema2json["Time"] = {"type": "string", "format": "datetime"}
+        schema2json["email"] = {
+            "oneOf": [{"type": "string"},
+                      {"type": "string", "format": "email"},
+                      {"type": "string", "format": "idn-email"}]}
+        schema2json["URL"] = {
+            "oneOf": [{"type": "string", "format": "uri"},
+                      {"type": "string", "format": "uri-reference"},
+                      {"type": "string", "format": "iri"},
+                      {"type": "string", "format": "iri-reference"}]}
     data = get_graph_item(schemaorg_data, prop_name)
     print('data', data, '\n')
     if ("@type" in data) and (data["@type"] == "rdfs:Class"):
@@ -482,7 +548,7 @@ def json_schema_from_schema_org2(schemaorg_data, vocabulary, draft='draft-04'):
                     if t not in schema["definitions"]:
                         missing_words.add(t)
         sys.stderr.write(f'finished: {word}\n')
-        return schema  # workaround for debugging
+        #return schema  # workaround for debugging
     if update_description:
         set_property(schema,
                      "description",
